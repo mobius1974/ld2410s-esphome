7dd8cd1b057e562996131bf5f085810cc385f61a
mobius1974 <148842740+mobius1974@users.noreply.github.com>
2025-10-16 13:04:02 +0700

    Update __init__.py

--- components/ld2410s/button/__init__.py
+++ components/ld2410s/button/__init__.py
@@ -2,71 +2,47 @@ import esphome.codegen as cg
 from esphome.components import button
 import esphome.config_validation as cv
 from esphome.const import (
-    ENTITY_CATEGORY_DIAGNOSTIC,
-    ENTITY_CATEGORY_CONFIG,
-    ICON_BUG,
-    DEVICE_CLASS_IDENTIFY,
-    DEVICE_CLASS_RESTART,
+    CONF_CALIBRATION,
+    CONF_FACTORY_RESET,
     DEVICE_CLASS_UPDATE,
-    ICON_RESTART_ALERT,
-    ICON_MEMORY
+    ENTITY_CATEGORY_CONFIG,
 )
+
 from .. import CONF_LD2410S_ID, LD2410S, ld2410s_ns
 
-LD2410SEnableConfigButton = ld2410s_ns.class_("LD2410SEnableConfigButton", button.Button)
-LD2410SDisableConfigButton = ld2410s_ns.class_("LD2410SDisableConfigButton", button.Button)
-LD2410SApplyConfigButton = ld2410s_ns.class_("LD2410SApplyConfigButton", button.Button)
-LD2410SAutoConfigThreshold = ld2410s_ns.class_("LD2410SAutoConfigThreshold", button.Button)
-
-CONF_ENABLE_CONFIG = "enable_config"
-CONF_DISABLE_CONFIG = "disable_config"
-CONF_APPLY_CONFIG = "apply_config"
-CONF_AUTO_THRESHOLD = "auto_threshold"
+LD2410SStartCalibrationButton = ld2410s_ns.class_(
+    "LD2410SStartCalibrationButton", button.Button
+)
+LD2410SFactoryResetButton = ld2410s_ns.class_(
+    "LD2410SFactoryResetButton", button.Button
+)
 
 CONFIG_SCHEMA = {
     cv.GenerateID(CONF_LD2410S_ID): cv.use_id(LD2410S),
-    cv.Optional(CONF_ENABLE_CONFIG): button.button_schema(
-        LD2410SEnableConfigButton,
-        device_class=DEVICE_CLASS_IDENTIFY,
-        entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
-        icon=ICON_BUG
-    ),
-    cv.Optional(CONF_DISABLE_CONFIG): button.button_schema(
-        LD2410SDisableConfigButton,
-        device_class=DEVICE_CLASS_IDENTIFY,
-        entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
-        icon=ICON_BUG
-    ),
-    cv.Required(CONF_APPLY_CONFIG): button.button_schema(
-        LD2410SApplyConfigButton,
-        device_class=DEVICE_CLASS_RESTART,
-        entity_category=ENTITY_CATEGORY_CONFIG,
-        icon=ICON_RESTART_ALERT,
-    ),
-    cv.Required(CONF_AUTO_THRESHOLD): button.button_schema(
-        LD2410SAutoConfigThreshold,
+    cv.Optional(CONF_CALIBRATION): button.button_schema(
+        LD2410SStartCalibrationButton,
         device_class=DEVICE_CLASS_UPDATE,
         entity_category=ENTITY_CATEGORY_CONFIG,
-        icon="mdi:icon-refresh",
-    )
+        icon="mdi:refresh-auto",
+    ),
+    cv.Optional(CONF_FACTORY_RESET): button.button_schema(
+        LD2410SFactoryResetButton,
+        device_class=DEVICE_CLASS_UPDATE,
+        entity_category=ENTITY_CATEGORY_CONFIG,
+        icon="mdi:factory",
+    ),
 }
+
 
 async def to_code(config):
     ld2410s_component = await cg.get_variable(config[CONF_LD2410S_ID])
-    if enable_confing := config.get(CONF_ENABLE_CONFIG):
-        b = await button.new_button(enable_confing)
+
+    if calibration := config.get(CONF_CALIBRATION):
+        b = await button.new_button(calibration)
         await cg.register_parented(b, config[CONF_LD2410S_ID])
-        cg.add(ld2410s_component.set_enable_config_button(b))
-    if disable_confing := config.get(CONF_DISABLE_CONFIG):
-        b = await button.new_button(disable_confing)
+        cg.add(ld2410s_component.set_calibration_button(b))
+
+    if factory_reset := config.get(CONF_FACTORY_RESET):
+        b = await button.new_button(factory_reset)
         await cg.register_parented(b, config[CONF_LD2410S_ID])
-        cg.add(ld2410s_component.set_disable_config_button(b))
-    if apply_config := config.get(CONF_APPLY_CONFIG):
-        b = await button.new_button(apply_config)
-        await cg.register_parented(b, config[CONF_LD2410S_ID])
-        cg.add(ld2410s_component.set_apply_config_button(b))
-    if auto_threshold := config.get(CONF_AUTO_THRESHOLD):
-        b = await button.new_button(auto_threshold)
-        await cg.register_parented(b, config[CONF_LD2410S_ID])
-        cg.add(ld2410s_component.set_auto_threshold_button(b))
-    
+        cg.add(ld2410s_component.set_factory_reset_button(b))
