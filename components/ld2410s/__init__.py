a0a3f5fae63c470b00c3a2b5517b72cf6d3ea83e
mobius1974 <148842740+mobius1974@users.noreply.github.com>
2025-10-16 12:52:02 +0700

    Update __init__.py

--- components/ld2410s/__init__.py
+++ components/ld2410s/__init__.py
@@ -1,9 +1,9 @@
 import esphome.codegen as cg
-import esphome.config_validation as cv
 from esphome.components import uart
+import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
-CODEOWNERS = ["@mrundead"]
+CODEOWNERS = ["@NovakIrs"]
 
 DEPENDENCIES = ["uart"]
 
