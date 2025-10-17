5949c58b3dab2a31f88f6ce3713d8d484cdabad1
mobius1974 <148842740+mobius1974@users.noreply.github.com>
2025-10-17 12:06:43 +0700

    Update ld2410s.cpp

--- components/ld2410s/ld2410s.cpp
+++ components/ld2410s/ld2410s.cpp
@@ -520,8 +520,7 @@ RxEvaluationResult LD2410Srx::receive_byte(uint32_t loop_count, uint8_t byte) {
 
     case RxEvaluationResult::NOK:
     default:
-      ESP_LOGE(TAG, "<XX [%d] %s < %s", loop_count, this->msg_.c_str(),
-               format_hex_pretty(this->rcv_buffer_, end_pos_ + 1, ' ').c_str());
+      //ESP_LOGE(TAG, "<XX [%d] %s < %s", loop_count, this->msg_.c_str(), format_hex_pretty(this->rcv_buffer_, end_pos_ + 1, ' ').c_str());
       this->reset_();
       result = RxEvaluationResult::UNKNOWN;
       break;
